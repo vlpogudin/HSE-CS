d572e768fdd43f15c777cb56014a436c393a0315
vlpogudin <vapogudin@edu.hse.ru>
2024-02-07 18:57:53 +0500

    new files added

---
+++ lab9_Dish/DishArray.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9_Dish
+{
+    public class DishArray
+    {
+        Dish[] arr;
+        public static int countObjects = 0;
+        public static int countCollections = 0;
+
+        static Random rnd = new Random();
+
+        public int Length
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+
+        #region Constructors
+        public DishArray() // parameterless constructor
+        {
+            arr = new Dish[3];
+            arr[0] = new Dish(1, 1, 1);
+            arr[1] = new Dish(2, 2, 2);
+            arr[2] = new Dish(3, 3, 3);
+            countObjects += 3;
+            countCollections++;
+        }
+
+        public DishArray(int Length) // parameterized constructor (random)
+        {
+            arr = new Dish[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                arr[i] = new Dish(rnd.Next(50), rnd.Next(50), rnd.Next(50));
+            }
+            countObjects += Length;
+            countCollections++;
+        }
+
+        public DishArray(int Length, int a) // parameterized constructor (manual input)
+        {
+            arr = new Dish[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                arr[i] = new Dish(Interface.GetDouble(1), Interface.GetDouble(2), Interface.GetDouble(3));
+            }
+            countObjects += Length;
+            countCollections++;
+        }
+
+        public DishArray(DishArray other) // copy constructor
+        {
+            arr = new Dish[other.Length];
+            for (int i = 0; i < Length; i++)
+            {
+                arr[i] = new Dish(other.arr[i]);
+            }
+            countObjects += Length;
+            countCollections++;
+        }
+        #endregion
+        public void Show()
+        {
+            for (int i = 0;i < this.Length;i++)
+            {
+                arr[i].Show();
+            }
+            Console.WriteLine();
+        }
+
+        public Dish this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arr.Length)
+                    return arr[index];
+
+                throw new IndexOutOfRangeException();
+            }
+
+            set 
+            {
+                if (index >= 0 && index < arr.Length)
+                    arr[index] = value;
+                else
+                    Interface.ChangeColor("Array index out of bounds!\n\n", ConsoleColor.Red);
+            }
+        }
+
+        public Dish FindMostCaloricFood()
+        {
+            Dish mostCaloric = arr[0];
+            for (int i = 1; i < this.Length; i++)
+            {
+                if (arr[i].NumberCalories() > mostCaloric.NumberCalories())
+                    mostCaloric = arr[i];
+            }
+            countObjects++;
+            return mostCaloric;
+        }
+    }
+}
---
+++ lab9_Dish/Program.cs
@@ -0,0 +1,190 @@
+﻿namespace lab9_Dish
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int answer;
+            do
+            {
+                Interface.PrintMainMenu();
+                answer = Interface.GetInt();
+                Console.Clear();
+                switch (answer)
+                {
+                    case 1:
+                        {
+                            Dish d1 = new Dish();
+                            string name = "nameless";
+                            d1.Show(name);
+                            Interface.PrintNumberCalories(d1.NumberCalories());
+
+                            Dish d2 = new Dish(27.8, 29.6, 1.7);
+                            name = "steak";
+                            d2.Show(name);
+                            Interface.PrintNumberCalories(d2.NumberCalories());
+
+                            Dish d3 = new Dish(d2);
+                            name = "beefsteak";
+                            d3.Show(name);
+                            Interface.PrintNumberCalories(Dish.CalculateCalories(d3));
+
+                            Interface.PrintCountObjects(1);
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Dish d1 = new Dish(3, 0.9, 13);
+                            string name = "draniki";
+                            d1.Show(name);
+                            Interface.PrintNumberCalories(d1.NumberCalories());
+
+                            d1++;
+                            d1.Show(name);
+                            Interface.PrintNumberCalories(d1.NumberCalories());
+
+                            Interface.PrintPercentageCalories(~d1);
+
+                            Dish d2 = new Dish(15, 15, 20);
+                            name = "idealDish";
+                            d2.Show(name);
+                            Interface.PrintNumberCalories(d2.NumberCalories());
+                            Interface.PrintIsIdeal((bool)d2);
+
+                            string dishDetails = d1;
+                            Interface.PrintDishDetails(dishDetails);
+
+                            int total = 1.5 * d1;
+                            Interface.PrintTotalCalorieContent(total);
+
+                            bool isHigher = d1 > d2;
+                            bool isLower = d1 < d2;
+                            Interface.PrintDishComparison(isHigher, isLower);
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            DishArray arr = new DishArray();
+                            int addAnswer;
+                            do
+                            {
+                                Interface.PrintAdditionalMenu();
+                                addAnswer = Interface.GetInt();
+                                switch (addAnswer)
+                                {
+                                    case 1:
+                                        {
+                                            Console.Clear();
+                                            arr = new DishArray(5);
+                                            arr.Show();
+                                            break;
+                                        }
+
+                                    case 2:
+                                        {
+                                            Console.Clear();
+                                            arr = new DishArray(5, 1);
+                                            Console.Clear();
+                                            arr.Show();
+                                            break;
+                                        }
+
+                                    case 3:
+                                        {
+                                            Console.Clear();
+                                            Interface.PrintMostCaloric(arr);
+                                            break;
+                                        }
+
+                                    case 4:
+                                        {
+                                            Console.Clear();
+                                            arr[0] = new(10, 10, 10);
+                                            Dish d = new(100, 100, 100);
+                                            arr[100] = d;
+                                            arr.Show();
+                                            try
+                                            {
+                                                Console.Write(arr[2]);
+                                            }
+                                            catch (IndexOutOfRangeException e)
+                                            {
+                                                Interface.ChangeColor(e.Message + "\n\n", ConsoleColor.Red);
+                                            }
+                                            try
+                                            {
+                                                Console.WriteLine(arr[100]);
+                                            }
+                                            catch (IndexOutOfRangeException e)
+                                            {
+                                                Interface.ChangeColor(e.Message + "\n\n", ConsoleColor.Red);
+                                            }
+                                            break;
+                                        }
+
+                                    case 5:
+                                        {
+                                            Interface.PrintCountObjects(2);
+                                            break;
+                                        }
+
+                                    case 6:
+                                        {
+                                            Console.Clear();
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            Console.Clear();
+                                            Interface.TypeError(2);
+                                            break;
+                                        }
+                                }
+                            } while (addAnswer != 6);
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            do
+                            {
+                                Interface.PrintExitMenu();
+                                answer = Interface.GetInt();
+                                Console.Clear();
+                                switch (answer)
+                                {
+                                    case 1:
+                                        {
+                                            Console.Clear();
+                                            Interface.ExitProgramm();
+                                            System.Environment.Exit(0);
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            Console.Clear();
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            Console.Clear();
+                                            Interface.TypeError(2);
+                                            break;
+                                        }
+                                }
+                            } while (answer != 2);
+                            break;
+                        }
+                    default:
+                        {
+                            Interface.TypeError(2);
+                            break;
+                        }
+                }
+            } while (answer != 4);
+        }
+    }
+}
